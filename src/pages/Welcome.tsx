70d40ea24bf9ac0e9a0809ee12895d596f657e6c
hiagopsilva <hiagohps360@gmail.com>
2021-07-21 17:50:28 -0300

    Initial commit

---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+import { View, Text } from 'react-native';
+// import { Wrapper } from './styles'; 
+
+type Props = {}
+
+const Welcome: FC<Props> = () => {
+  return <View><Text>Olá</Text></View>;
+};
+
+export default Welcome;
