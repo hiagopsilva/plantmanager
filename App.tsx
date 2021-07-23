0ffa2e3c0009883bd42297fe9774215f445eff0a
hiagopsilva <hiagohps360@gmail.com>
2021-07-22 21:58:53 -0300

    chore: updated

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import AppLoading from 'expo-app-loading'
 
 import { Welcome } from './src/pages/Welcome';
+import { UserIdentification } from './src/pages/UserIdentification';
 import { useFonts, Jost_400Regular, Jost_600SemiBold } from '@expo-google-fonts/jost';
 
 export default function App() {
@@ -13,6 +14,6 @@ export default function App() {
   if (!fontsLoaded) return <AppLoading />
 
   return (
-    <Welcome />
+    <UserIdentification />
   );
 }
