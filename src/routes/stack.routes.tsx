57ca2460826519915a52f91bb37ccfaa0a6874dc
hiagopsilva <hiagohps360@gmail.com>
2021-07-29 12:20:31 -0300

    feat: routes plant select

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -4,6 +4,8 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { Welcome } from '../pages/Welcome';
 import { Confirmation } from '../pages/Confirmation';
 import { UserIdentification } from '../pages/UserIdentification';
+import { PlantSelect } from '../pages/PlantSelect';
+
 import colors from '../styles/colors';
 
 const StackRoutes = createStackNavigator();
@@ -26,6 +28,11 @@ const AppRoutes: React.FC = () => {
       <StackRoutes.Screen
         name="Confirmation" 
         component={Confirmation}
+      />
+
+      <StackRoutes.Screen
+        name="PlantSelect" 
+        component={PlantSelect}
       />
     </StackRoutes.Navigator>
   );
