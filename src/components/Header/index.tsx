78ee5704397226d819eb07cb396f5951128bf170
hiagopsilva <hiagohps360@gmail.com>
2021-08-03 19:22:44 -0300

    feat: get name async storage

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,26 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { Wrapper, Content, Greeting, UserName, Image } from './styles'; 
 import image from '../../assets/image.png'
 
 const Header: FC = () => {
+  const [userName, setUserName] = useState<string>();
+
+  useEffect(() => {
+    const loadStorageUserName = async () => {
+      const user = await AsyncStorage.getItem('@plantmanager:user')
+
+      setUserName(user || '')
+    }
+
+    loadStorageUserName();
+  },[]);
   return (
     <Wrapper>
       <Content>
         <Greeting>Olá</Greeting>
-        <UserName>Hiago</UserName>
+        <UserName>{userName}</UserName>
       </Content>
 
       <Image source={image} />
