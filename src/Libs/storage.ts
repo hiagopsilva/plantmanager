c4197efecaa0e81dce7eccbde424014f5b5164eb
hiagopsilva <hiagohps360@gmail.com>
2021-08-04 15:57:08 -0300

    feat: method save plants

--- src/Libs/storage.ts
+++ src/Libs/storage.ts
@@ -15,8 +15,41 @@ export interface PlantProps {
   dateTimeNotification: Date;
 }
 
-export interface a {
+export interface StoragePlantProps {
   [id: string]: {
     data: PlantProps;
+  }
+}
+
+export async function savePlant(plant: PlantProps):Promise<void> {
+  try {
+    const data = await AsyncStorage.getItem('@plantmanager:plants')
+    const oldPlants = data ? (JSON.parse(data) as StoragePlantProps) : {}
+
+    const newPlant = {
+      [plant.id]: {
+        data: plant
+      }
+    }
+
+    await AsyncStorage.setItem('@plantmanager:plants',
+    JSON.stringify({
+      ...newPlant,
+      ...oldPlants
+    }))
+
+  } catch(error){
+    throw new Error(error)
+  }
+}
+
+export async function loadPlant():Promise<StoragePlantProps> {
+  try {
+    const data = await AsyncStorage.getItem('@plantmanager:plants')
+    const plants = data ? (JSON.parse(data) as StoragePlantProps) : {}
+
+    return plants;
+  } catch(error){
+    throw new Error(error)
   }
 }
