81670ab07631702929009afacbfaf1ef1354407a
hiagopsilva <hiagohps360@gmail.com>
2021-08-10 23:04:58 -0300

    fix: tab navigation

--- src/routes/tab.routes.tsx
+++ src/routes/tab.routes.tsx
@@ -12,41 +12,41 @@ const AppTab = createBottomTabNavigator();
 const AuthRoutes = () => {
   return(
     <AppTab.Navigator
-      // tabBarOptions={{
-        // activeTintColor: colors.green,
-        // inactiveTintColor: colors.heading,
-        // labelPosition: 'beside-icon',
-        // style: {
-        //   paddingVertical: 20,
-        //   height: 88
-        // },
-      // }}
+      tabBarOptions={{
+        activeTintColor: colors.green,
+        inactiveTintColor: colors.heading,
+        labelPosition: 'beside-icon',
+        style: {
+          paddingVertical: 20,
+          height: 88
+        },
+      }}
     >
       <AppTab.Screen 
         name="Nova"
         component={PlantSelect}
-        // options={{
-        //   tabBarIcon: (({ size, color }) => (
-        //     <MaterialIcons
-        //       name="add-circle-outline"
-        //       size={size}
-        //       color={color}
-        //     />
-        //   ))
-        // }}
+        options={{
+          tabBarIcon: (({ size, color }) => (
+            <MaterialIcons
+              name="add-circle-outline"
+              size={size}
+              color={color}
+            />
+          ))
+        }}
       />
       <AppTab.Screen 
         name="Minhas"
         component={MyPlants}
-        // options={{
-        //   tabBarIcon: (({ size, color }) => (
-        //     <MaterialIcons
-        //       name="format-list-bulleted"
-        //       size={size}
-        //       color={color}
-        //     />
-        //   ))
-        // }}
+        options={{
+          tabBarIcon: (({ size, color }) => (
+            <MaterialIcons
+              name="format-list-bulleted"
+              size={size}
+              color={color}
+            />
+          ))
+        }}
       />
     </AppTab.Navigator>
   )
